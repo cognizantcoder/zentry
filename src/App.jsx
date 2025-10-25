88bc9bd476d7ec8015e4627c3b92c0376dbcbbc4
Sumit <Sumitgiri112341@outlook.com>
2025-10-25 19:41:14 +0530

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 import HeroSection from "./Component/Hero";
 import About from "./Component/About";
+import Navbar from "./Component/Navbar";
+import Features from "./Component/Features";
+import Story from "./Component/Story";
+import Contact from "./Component/Contact";
+import Footer from "./Component/Footer";
 
 const App = () => {
   return (
-    <main className='relative min-h-screen w-screen overflow-x-hidden'>
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+      <Navbar />
       <HeroSection />
-      <About/>
-    <section  className=" z-0 min-h-screen "/>
+      <About />
+      <Features />
+      <Story />
+      <Contact />
+      <Footer />
     </main>
   );
 };
